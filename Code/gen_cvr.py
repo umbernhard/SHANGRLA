5d3727d1afc33c609bbefa6425224ad56fa26d69
umbernhard <umbernhard@gmail.com>
2020-02-21 11:34:04 -0500

    PEP-8 conformance changes

--- Code/gen_cvr.py
+++ Code/gen_cvr.py
@@ -1,71 +1,72 @@
 """
-Outputs "fake" CVR for consumption by SHANGRLA for elections that are 
-conducted without CVRs. Note that the need for a CVR where there isn't one 
-means that we can't audit more than one race at a time with SHANGRLA, since we 
-can't know which combination of votes appears on which ballot. 
+Outputs "fake" CVR for consumption by SHANGRLA for elections that are
+conducted without CVRs. Note that the need for a CVR where there isn't one
+means that we can't audit more than one race at a time with SHANGRLA, since we
+can't know which combination of votes appears on which ballot.
 """
 
-import csv, os, time, sys
+import csv
+import os
+import time
+import sys
 
 from assertion_audit_utils import CVR
 
-interactive = True
+
+def main():
+    interactive = True
 
 
-if len(sys.argv) > 1:
-    interactive = False
+    if len(sys.argv) > 1:
+        interactive = False
 
-    if sys.argv[1] == '-t':
-        if len(sys.argv) != 3:
-            print('Usage: python3 gen_cvr.py -t [res_file_name].csv')
-            sys.exit(1)
-        res_file_name = sys.argv[2]
-        
+        if sys.argv[1] == '-t':
+            if len(sys.argv) != 3:
+                print('Usage: python3 gen_cvr.py -t [res_file_name].csv')
+                sys.exit(1)
+            res_file_name = sys.argv[2]
 
-while interactive:
-    print('Please specify an election results CSV file of the form:')
-    print('\tCandidate Name,Number of Votes')
-    print()
-    print('E.g.,')
-    print('\tCandidate Name, Number of Votes')
-    print('\tAlice, 634')
-    print('\tBob, 321')
-    print('\tCharlie, 13')
-    print('\tTotal Votes, 1000')
-    print()
-    print('Input the file name of the results file: ')
 
-    res_file_name = input()
-    
-    if os.path.exists(res_file_name):
-        break
-    else:
+    while interactive:
+        print('Please specify an election results CSV file of the form:')
+        print('\tCandidate Name,Number of Votes')
+        print()
+        print('E.g.,')
+        print('\tCandidate Name, Number of Votes')
+        print('\tAlice, 634')
+        print('\tBob, 321')
+        print('\tCharlie, 13')
+        print('\tTotal Votes, 1000')
+        print()
+        print('Input the file name of the results file: ')
+
+        res_file_name = input()
+
+        if os.path.exists(res_file_name):
+            break
+
         print('Specified file {} does not exist'.format(res_file_name))
         print()
         time.sleep(1)
 
-res = {}
-total_votes = 0
-try: 
-    for line in csv.DictReader(open(res_file_name)):
-        if line['Candidate Name'] == 'Total Votes':
-            total_votes = int(line['Number of Votes'])
-        else:
-            cand = line['Candidate Name']
-            votes = int(line['Number of Votes'])
+    res = {}
+    total_votes = 0
+    try:
+        for line in csv.DictReader(open(res_file_name)):
+            if line['Candidate Name'] == 'Total Votes':
+                total_votes = int(line['Number of Votes'])
+            else:
+                cand = line['Candidate Name']
+                votes = int(line['Number of Votes'])
 
-            res[cand] = votes
-
-
-except Exception  as e:
-    print('Results file is mal-formatted: {}'.format(e))
-    sys.exit(1)
-
-assert sum(res.values()) <= total_votes, 'Results file shows more votes than reported!'
+                res[cand] = votes
 
 
+    except Exception as ex:
+        print('Results file is mal-formatted: {}'.format(ex))
+        sys.exit(1)
 
+    assert sum(res.values()) <= total_votes, 'Results file shows more votes than reported!'
 
-
-
-
+if __name__ == '__main__':
+    main()
